1030535f49c997f861a48255f89a1e126a19483e
Rubalito <adrian.perez.garcia2018@gmail.com>
2021-01-27 15:32:14 +0100

    Animaciones y Asincronia

--- _JS/_21_asyncrony/index.js
+++ _JS/_21_asyncrony/index.js
@@ -2,11 +2,33 @@ const throwDice = (callback) =>
   setTimeout(() => callback(Math.ceil(Math.random() * 5) + 1), 1000);
 
 const getPlayerScore = (data) => {
-    throwDice((num1) => {
-        throwDice((num2) => {
-            data([num1,num2])
-        })
-    })
+  throwDice((num1) => {
+    throwDice((num2) => {
+      data([num1, num2]);
+    });
+  });
 };
 
-getPlayerScore((data) => console.log(data));
+getPlayerScore((result) =>
+    getPlayerScore((result2) => {
+        getPlayerScore((result3) => {
+            console.log([result, result2, result3])
+        })
+    })
+);
+
+const startGame = (players) => {
+  let promises = [];
+  for (let i = 1; i <= players; i++) {
+    getPlayerScore((result) => {
+      promises.push(result);
+      if (promises.length == players) {
+        Promise.all(promises).then((values) => {
+          console.log(values);
+        });
+      }
+    });
+  }
+};
+
+startGame(6);
